797a87170cceb6fdac6d220af15ee4500456d243
Krishna Pradeep kumar Kadarla <49063654+kpkk@users.noreply.github.com>
2026-09-06 13:56:06 +0530

    Add files via upload

--- week-10-problems/TreeImpl.java
+++ week-10-problems/TreeImpl.java
@@ -1,7 +1,9 @@
 package week10;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Node {
         int data;
@@ -78,6 +80,70 @@ class Node {
         inOrder(root.right, list);
     }
 
+    // https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+    public List<List<Integer>> levelOrderTraversal(Node root){
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> ans = new LinkedList<>();
+        if(root==null) return ans;
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            List<Integer> list = new LinkedList<>();
+            int level = queue.size();
+            for (int i=0;i<level;i++){
+                Node poll = queue.poll();
+                if(poll.left!=null) queue.offer(poll.left);
+                if(poll.right!=null) queue.offer(poll.right);
+                list.add(poll.data);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+
+    // https://leetcode.com/problems/same-tree/description/
+    public boolean isSameTree(Node p, Node q) {
+            if(p==null && q==null) return true;
+            if(p==null || q==null || p.data!=q.data) return false;
+            boolean left=isSameTree(p.left, q.left);
+            boolean right=isSameTree(p.right, q.right);
+            return left && right;
+
+    }
+
+    // https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    public int maxDepth(Node root) {
+            if(root==null) return 0;
+            int left= maxDepth(root.left);
+            int right= maxDepth(root.right);
+            return 1+ Math.max(left, right);
+    }
+
+    // https://leetcode.com/problems/path-sum/
+        public boolean hasPathSum(Node root, int targetSum) {
+            return dfs(root, targetSum);
+        }
+
+        public boolean dfs(Node root, int target){
+            if(root==null) return false;
+            if(root.left==null && root.right==null) return target-root.data==0;
+            boolean left = dfs(root.left, target-root.data);
+            boolean right = dfs(root.right, target-root.data);
+            return left || right;
+        }
+
+        //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+
+        public Node lowestCommonAncestor(Node root, Node p, Node q) {
+            if(root==null || root==p || root==q) return root;
+            Node left = lowestCommonAncestor(root.left, p, q);
+            Node right = lowestCommonAncestor(root.right, p, q);
+            if(left==null)
+                return right;
+            else if(right==null)
+                return left;
+            else return root;
+        }
+
     public static void main(String[] args) {
         TreeImpl tree = new TreeImpl(1);
         tree.root.left= new Node(2);
@@ -100,5 +166,8 @@ class Node {
         tree.preOrderTraversal(tree.root);
         tree.postOrderTraversal(tree.root);
         tree.inOrderTraversal(tree.root);
+
+        List<List<Integer>> list = tree.levelOrderTraversal(tree.root);
+        System.out.println(list);
     }
 }
